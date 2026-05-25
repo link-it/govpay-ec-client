cddf9d473cfe0ebcaee82abec78101fc7eaea48b
Giuliano Pintori <pintori@link.it>
2026-05-25 14:51:44 +0200

    Issue #4 Migrazione a Spring Boot 4.x / Spring Framework 7.x.
    Aggiornato il parent govpay-bom a 2.0.0-SNAPSHOT e avanzata la versione
    del progetto da 1.0.0 a 2.0.0-SNAPSHOT. Il nuovo BOM gestisce Spring Boot
    4.0.6, Spring Framework 7.0.7, Spring Security 7.0.5, Jackson 3.1.3
    (groupId tools.jackson) e SpringDoc OpenAPI 3.0.3.
    
    Aggiornata la configurazione di openapi-generator-maven-plugin con i
    flag useSpringBoot4 e useJackson3, openApiNullable=false e sourceFolder
    unico. Aggiunto build-helper-maven-plugin per registrare i sorgenti
    generati. Rimosse le dipendenze esplicite jackson-databind,
    jackson-datatype-jsr310 e jackson-databind-nullable (ora gestite via
    BOM con tools.jackson e autodiscovery del modulo datetime).
    
    Override Netty a 4.2.14.Final per risolvere CVE-2026-42577, CVE-2026-42578,
    CVE-2026-42579, CVE-2026-42580, CVE-2026-42581, CVE-2026-42582,
    CVE-2026-42583, CVE-2026-42584, CVE-2026-42585, CVE-2026-42586,
    CVE-2026-42587, CVE-2026-44248, CVE-2026-41417. Spring Boot 4.0.6 importa
    Netty 4.2.12.Final, la 4.2.14.Final include i fix.
    
    Aggiornati i test per Jackson 3 (tools.jackson.databind.ObjectMapper) e
    aggiunti gli header di copyright GPL v3 alle classi Java sorgente.

--- src/test/java/it/govpay/ec/client/beans/SoggettoTest.java
+++ src/test/java/it/govpay/ec/client/beans/SoggettoTest.java
@@ -1,9 +1,26 @@
+/*
+ * GovPay - Porta di Accesso al Nodo dei Pagamenti SPC
+ * http://www.gov4j.it/govpay
+ *
+ * Copyright (c) 2014-2026 Link.it srl (http://www.link.it).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.govpay.ec.client.beans;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tools.jackson.databind.ObjectMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +31,6 @@ class SoggettoTest {
     @BeforeEach
     void setUp() {
         objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
     }
 
     @Test
